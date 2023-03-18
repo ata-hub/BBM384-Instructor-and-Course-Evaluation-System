3e05cfea8c9abafafd81e9dfdaf8a3627ae550cb
Acymtoth <hasan258225@yandex.com>
2023-03-18 23:48:21 +0300

    firstcommit
    here is the beggining of our project
    -hasan

---
+++ backend/bbm384/src/main/java/com/example/bbm384/Bbm384Application.java
@@ -0,0 +1,13 @@
+package com.example.bbm384;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bbm384Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bbm384Application.class, args);
+	}
+
+}
---
+++ backend/bbm384/src/test/java/com/example/bbm384/Bbm384ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bbm384;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bbm384ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
